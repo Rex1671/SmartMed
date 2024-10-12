0e55d65c4f1ae18b59081a9649640beb51865ca7
Rex1671 <iamrakeshraut@gmail.com>
2024-10-13 01:39:37 +0530

    Initial commit

---
+++ HomePage/webs.js
@@ -0,0 +1,19 @@
+const profileButton = document.getElementById('profileButton'); 
+profileButton.addEventListener('click', () => {
+    window.location.href = 'profile.html'; 
+});
+
+const userId = localStorage.getItem('loggedInUserId');
+console.log("User id"+userId)
+
+document.getElementById('symptomAnalysisCard').addEventListener('click', function() {
+    window.location.href = '../Symptom/index.html';
+});
+
+document.getElementById('treatmentPlanCard').addEventListener('click', function() {
+    window.location.href = '../PatientHistory/PH.html';
+});
+
+document.getElementById('syntheticDataCard').addEventListener('click', function() {
+    window.location.href = '../SMD/index.html';
+});
