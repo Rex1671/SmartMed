afe5223822ccd40eb286903f7f1e76117883384e
Rex1671 <150885880+Rex1671@users.noreply.github.com>
2024-10-13 10:44:07 +0530

    Update webs.js

--- HomePage/webs.js
+++ HomePage/webs.js
@@ -1,19 +1,19 @@
 const profileButton = document.getElementById('profileButton'); 
 profileButton.addEventListener('click', () => {
-    window.location.href = 'profile.html'; 
+    window.location.href = 'Profile/profile.html'; 
 });
 
 const userId = localStorage.getItem('loggedInUserId');
 console.log("User id"+userId)
 
 document.getElementById('symptomAnalysisCard').addEventListener('click', function() {
-    window.location.href = '../Symptom/index.html';
+    window.location.href = 'Symptom/index.html';
 });
 
 document.getElementById('treatmentPlanCard').addEventListener('click', function() {
-    window.location.href = '../PatientHistory/PH.html';
+    window.location.href = 'PatientHistory/PH.html';
 });
 
 document.getElementById('syntheticDataCard').addEventListener('click', function() {
-    window.location.href = '../SMD/index.html';
+    window.location.href = 'SMD/index.html';
 });
