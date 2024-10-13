5e0e775c419f3be9dfa029825b4fb1735d97ff4d
Rex1671 <150885880+Rex1671@users.noreply.github.com>
2024-10-13 10:53:45 +0530

    Update webs.js

--- HomePage/webs.js
+++ HomePage/webs.js
@@ -1,19 +1,19 @@
 const profileButton = document.getElementById('profileButton'); 
 profileButton.addEventListener('click', () => {
-    window.location.href = 'Profile/profile.html'; 
+    window.location.href = 'https://rex1671.github.io/SmartMed/Profile/profile.html'; 
 });
 
 const userId = localStorage.getItem('loggedInUserId');
 console.log("User id"+userId)
 
 document.getElementById('symptomAnalysisCard').addEventListener('click', function() {
-    window.location.href = 'Symptom/index.html';
+    window.location.href = 'https://rex1671.github.io/SmartMed/Symptom/index.html';
 });
 
 document.getElementById('treatmentPlanCard').addEventListener('click', function() {
-    window.location.href = 'PatientHistory/PH.html';
+    window.location.href = 'https://rex1671.github.io/SmartMed/PatientHistory/PH.html';
 });
 
 document.getElementById('syntheticDataCard').addEventListener('click', function() {
-    window.location.href = 'SMD/index.html';
+    window.location.href = 'https://rex1671.github.io/SmartMed/SMD/index.html';
 });
